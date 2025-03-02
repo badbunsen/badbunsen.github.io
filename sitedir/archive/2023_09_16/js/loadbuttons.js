aab4ad54d4124376c6c1e38959bb42c55b092ee5
Aleph Diallo <aleph@badbunsen.com>
2025-03-02 13:37:28 -0500

    move archive and neocities repos to this one for easy access

---
+++ sitedir/archive/2023_09_16/js/loadbuttons.js
@@ -0,0 +1,10 @@
+//Import buttons
+var button1 = "<a href='/archive/2023_09_16/'><img src='/archive/2023_09_16/images/buttons/home.svg' alt='homepage' width='auto' height='100%'></a>";
+var button2 = "<a href='/archive/2023_09_16/cartoons'><img src='/archive/2023_09_16/images/buttons/cartoons.svg' alt='cartoons' width='auto' height='100%'></a>";
+var button3 = "<a href='/archive/2023_09_16/artbook'><img src='/archive/2023_09_16/images/buttons/artbook.svg' alt='artbook' width='auto' height='100%'></a>";
+var button4 = "<a href='/archive/2023_09_16/games'><img src='/archive/2023_09_16/images/buttons/games.svg' alt='games' width='auto' height='100%'></a>";
+var button5 = "<a href='/archive/2023_09_16/work'><img src='/archive/2023_09_16/images/buttons/work.svg' alt='work' width='auto' height='100%'></a>";
+var button6 = "<a href='/archive/2023_09_16/goodies'><img src='/archive/2023_09_16/images/buttons/downloads.svg' width='auto' alt='downloads' height='100%'></a>";
+var button7 = "<a href='/archive/2023_09_16/about'><img src='/archive/2023_09_16/images/buttons/about.svg' alt='about' width='auto' height='100%'></a>";
+var buttonspan = document.getElementById("buttons");
+buttonspan.innerHTML = button1 + button2 + button3 + button4 + button5 + button6 + button7;
